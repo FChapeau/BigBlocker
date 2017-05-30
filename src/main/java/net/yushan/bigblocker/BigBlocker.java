b9be8e70b074a8c64ffc23a145f27828664bc77b
François Drouin-Morin <francois.drouinm@gmail.com>
2017-05-29 20:47:28 -0400

    Initial commit

---
+++ src/main/java/net/yushan/bigblocker/BigBlocker.java
@@ -0,0 +1,34 @@
+package net.yushan.bigblocker;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = BigBlocker.MODID, version = BigBlocker.VERSION)
+public class BigBlocker
+{
+    public static final String MODID = "bigblocker";
+    public static final String VERSION = "0.1";
+
+    @SidedProxy(clientSide="net.yushan.bigblocker.ClientProxy", serverSide="net.yushan.bigblocker.ServerProxy")
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+        proxy.preInit(e);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e) {
+        proxy.init(e);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+        proxy.postInit(e);
+    }
+}
---
+++ src/main/java/net/yushan/bigblocker/CommonProxy.java
@@ -0,0 +1,22 @@
+package net.yushan.bigblocker;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by francois.drouin on 2017-05-29.
+ */
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent e) {
+
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
